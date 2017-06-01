4e1940f205978909c52316adf2bdec7a2bb7751b
j-hughes <jhughes70@gatech.edu>
2017-06-01 15:10:45 -0400

    Fix Ant Lab Main Hopefully

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -67,7 +67,6 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("P3");
 		  toPrint += p3.toString("gburdell3");
-<<<<<<< HEAD
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("Jordan Hughes");
@@ -76,20 +75,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Prit Shah");
                   toPrint += p5.toString("pshah334");
-=======
-          //Person4 replace P4 with your name
-          //and gburdell4 with your gt id
-          Person4 p4 = new Person4("P4");
-          toPrint += p4.toString("gburdell4");
-          //Person5 replace P5 with your name
-          //and gburdell5 with your gt id
-          Person5 p5 = new Person5("Prit Shah");
-          toPrint += p5.toString("pshah334");
->>>>>>> 3118df1b6592b048bcaea6b21674e00be5415377
-
         System.out.println(toPrint);
-
-
     }
 
 
