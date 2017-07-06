553d5744af5bf1c9599ffd968ddb5ad81ba9ef07
pritshah98 <addison20>
2017-07-06 19:03:33 -0400

    Initialization

---
+++ WheresMyStuff/app/src/main/java/com/example/seekers/wheresmystuff/Person.java
@@ -0,0 +1,62 @@
+package com.example.seekers.wheresmystuff;
+
+/**
+ * Created by Marcus on 6/20/2017.
+ */
+
+/**
+ * A class that represents a person(user or administrator)
+ */
+public abstract class Person {
+    /**
+     * getter method for the name
+     * @return the name of the person
+     */
+    public abstract String getName();
+
+    /**
+     * method to check if a User is banned
+     */
+    public abstract boolean isBanned();
+
+    /**
+     * getter method for the username
+     * @return the username of the person
+     */
+    public abstract String getUsername();
+
+    /**
+     * getter method for the AcccountType
+     * @return the accounttype for the person
+     */
+    public abstract String getAccountType();
+
+    /**
+     * the getter method for the password
+     * @return the password for the person
+     */
+    public abstract String getPassword();
+
+    /**
+     * get number incorrect tries
+     * @return number incorrect tries
+     */
+    public abstract int getIncorrect();
+
+    /**
+     * reset number of attempts to login
+     */
+    public abstract void resetIncorrect();
+
+    /**
+     * add to number of attempts to login
+     */
+    public abstract void addIncorrect();
+
+    /**
+     * ovverides the tostring method for this class
+     * @return a string that properly describes the information held in the
+     * class
+     */
+    public abstract String toString();
+}
