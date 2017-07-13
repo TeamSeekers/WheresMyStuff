3cb07000d3aa6d10878df23b8462be4cf7e087f3
lukemiller1 <lmiller64@gatech.edu>
2017-07-13 09:26:57 -0400

    Update FoundItemsListActivity.java

--- WheresMyStuff/app/src/main/java/com/example/seekers/wheresmystuff/Controller/FoundItemsListActivity.java
+++ WheresMyStuff/app/src/main/java/com/example/seekers/wheresmystuff/Controller/FoundItemsListActivity.java
@@ -8,8 +8,6 @@ import android.widget.Button;
 import android.widget.ListView;
 import com.example.seekers.wheresmystuff.R;
 
-import com.example.seekers.wheresmystuff.R;
-
 /**
  * The controller class that represents when someone needs to view a list
  * of found items.
