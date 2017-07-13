95c67d27a80e485338de89d30a081452785c85f3
pritshah98 <addison20>
2017-07-13 13:55:58 -0400

    added registration username and password logic

--- WheresMyStuff/app/src/main/java/com/example/seekers/wheresmystuff/Controller/RegistrationScreenActivity.java
+++ WheresMyStuff/app/src/main/java/com/example/seekers/wheresmystuff/Controller/RegistrationScreenActivity.java
@@ -50,31 +50,45 @@ public class RegistrationScreenActivity extends AppCompatActivity {
                 String newPassword = enterPassword.getText().toString();
                 String code = enterCode.getText().toString();
 
-                if (userAccountType.isChecked()) {
-                    account = "User";
-                    User newUser = new User(enterName.getText().toString(),
-                            newUserName, newPassword, account);
-                    WelcomeScreenActivity.myRef.child("Users").child(newUserName).setValue(newUser);
-                    WelcomeScreenActivity.personList.getPersonList().put(newUser.getUsername(), newUser);
-                    showAlert();
-                    finish();
-                    overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+                if (WelcomeScreenActivity.personList.getPersonList().containsValue(newUserName)) {
+                    AlertDialog.Builder builder1 = new AlertDialog.Builder(RegistrationScreenActivity.this);
+                    builder1.setMessage("Username already taken.");
+                    builder1.setCancelable(true);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
+                } else if (newPassword.length() < 5) {
+                    AlertDialog.Builder builder1 = new AlertDialog.Builder(RegistrationScreenActivity.this);
+                    builder1.setMessage("Password length must be greater than 4.");
+                    builder1.setCancelable(true);
+                    AlertDialog alert11 = builder1.create();
+                    alert11.show();
                 } else {
-                    if (code.equals("cs2340")) {
-                        account = "Admin";
-                        Admin newAdmin = new Admin(enterName.getText().toString(),
+                    if (userAccountType.isChecked()) {
+                        account = "User";
+                        User newUser = new User(enterName.getText().toString(),
                                 newUserName, newPassword, account);
-                        WelcomeScreenActivity.myRef.child("Admins").child(newUserName).setValue(newAdmin);
-                        WelcomeScreenActivity.personList.getPersonList().put(newAdmin.getUsername(), newAdmin);
+                        WelcomeScreenActivity.myRef.child("Users").child(newUserName).setValue(newUser);
+                        WelcomeScreenActivity.personList.getPersonList().put(newUser.getUsername(), newUser);
                         showAlert();
                         finish();
                         overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
                     } else {
-                        AlertDialog.Builder builder1 = new AlertDialog.Builder(RegistrationScreenActivity.this);
-                        builder1.setMessage("Incorrect code entered.");
-                        builder1.setCancelable(true);
-                        AlertDialog alert11 = builder1.create();
-                        alert11.show();
+                        if (code.equals("cs2340")) {
+                            account = "Admin";
+                            Admin newAdmin = new Admin(enterName.getText().toString(),
+                                    newUserName, newPassword, account);
+                            WelcomeScreenActivity.myRef.child("Admins").child(newUserName).setValue(newAdmin);
+                            WelcomeScreenActivity.personList.getPersonList().put(newAdmin.getUsername(), newAdmin);
+                            showAlert();
+                            finish();
+                            overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+                        } else {
+                            AlertDialog.Builder builder1 = new AlertDialog.Builder(RegistrationScreenActivity.this);
+                            builder1.setMessage("Incorrect code entered.");
+                            builder1.setCancelable(true);
+                            AlertDialog alert11 = builder1.create();
+                            alert11.show();
+                        }
                     }
                 }
             }
