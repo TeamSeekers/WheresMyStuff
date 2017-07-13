2ff2f9a6c80bc35216f4bd739a927b7a1ce90b86
lukemiller1 <lmiller64@gatech.edu>
2017-07-13 09:11:47 -0400

    Interface Changes and Design Changes along w/ Animations
    Added two types of animations, design pattern animations which is a
    sliding transition between views in app depending on whether transition
    is going up or down the heirarchy. And the other is a visual effect
    animation, or a splash screen, that occurs at bootup time. Also
    reformatted the Home Screen for improved UI and not as cluttered.
    Seperated all actions into three tabs (Lost, Found, and Misc) and is
    open to suggestions/improvement as well.

---
+++ WheresMyStuff/app/src/main/java/com/example/seekers/wheresmystuff/Controller/FoundItemsListActivity.java
@@ -0,0 +1,39 @@
+package com.example.seekers.wheresmystuff.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import com.example.seekers.wheresmystuff.R;
+
+/**
+ * The controller class that represents when someone needs to view a list
+ * of found items.
+ */
+public class FoundItemsListActivity extends AppCompatActivity {
+
+    private ListView itemsList;
+    private ArrayAdapter adapter;
+    private Button home;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_found_items_list);
+        itemsList = (ListView) findViewById(R.id.itemsList);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, WelcomeScreenActivity.foundItemList.getFoundItemList());
+        itemsList.setAdapter(adapter);
+        home = (Button) findViewById(R.id.backToHomeSearch);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+            }
+        });
+    }
+}
---
+++ WheresMyStuff/app/src/main/java/com/example/seekers/wheresmystuff/Controller/LostItemsListActivity.java
@@ -0,0 +1,45 @@
+package com.example.seekers.wheresmystuff.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import com.example.seekers.wheresmystuff.R;
+
+/**
+ * Controller class for when someone would like to view a list of lost
+ * items.
+ */
+public class LostItemsListActivity extends AppCompatActivity {
+
+    private ListView itemsList;
+    private ArrayAdapter adapter;
+    private Button home;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_items_list);
+        itemsList = (ListView) findViewById(R.id.itemsList);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, WelcomeScreenActivity.lostItemList.getLostItemList());
+        itemsList.setAdapter(adapter);
+        home = (Button) findViewById(R.id.backToHomeSearch);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
+    }
+}
