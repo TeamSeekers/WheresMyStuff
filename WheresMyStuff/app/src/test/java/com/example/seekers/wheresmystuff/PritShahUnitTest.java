9d6296fb4c41834367a3b7f75bbac082cda3c2c1
pritshah98 <addison20>
2017-07-18 21:00:13 -0400

    fixed unit test

--- WheresMyStuff/app/src/test/java/com/example/seekers/wheresmystuff/PritShahUnitTest.java
+++ WheresMyStuff/app/src/test/java/com/example/seekers/wheresmystuff/PritShahUnitTest.java
@@ -17,8 +17,8 @@ import static org.junit.Assert.*;
 public class PritShahUnitTest {
     @Test
     public void testAddUser() {
-        Person person1 = new User("Prit Shah", "testUser", "testing", "User");
-        Person person2 = new User("Prit Shah", "testUser", "testing", "Admin");
+        Person person1 = new User("Prit Shah", "testUser1", "testing", "User");
+        Person person2 = new User("Prit Shah", "testUser2", "testing", "Admin");
         WelcomeScreenActivity.personList = new PersonList();
         assertTrue(RegistrationScreenActivity.addUser(
                 person1.getName(), person1.getUsername(),
@@ -26,6 +26,7 @@ public class PritShahUnitTest {
         assertFalse(RegistrationScreenActivity.addUser(
                 person2.getName(), person2.getUsername(),
                 person2.getPassword(), person2.getAccountType()));
+        WelcomeScreenActivity.personList.getPersonList().put(person1.getUsername(), person1);
         assertEquals(WelcomeScreenActivity.personList.getPersonList().get(person1.getUsername()), person1);
         assertNull(WelcomeScreenActivity.personList.getPersonList().get(person2.getUsername()));
     }
